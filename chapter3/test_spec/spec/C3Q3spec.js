90a57b3f655d3e91bae8e4246cd457c071fb512a
Magnus01 <magnus01lysfjord@gmail.com>
2016-08-05 07:08:51 +0100

    Updated C3Q3 to template

--- chapter3/test_spec/spec/C3Q3spec.js
+++ chapter3/test_spec/spec/C3Q3spec.js
@@ -1,11 +1,43 @@
-//infix expression should be ["4", "+", "5", "*", "2"]
-var Cha3Q3 = require("../src/C3Q3.js");
-describe("multiplication", function () {
-  it("should multiply 2 and 3", function () {
+var calculator = require("../src/C3Q3.js");
 
-    var product = Cha3Q3.printStack();
-    expect(product).toEqual([3, 5, 2]);
-  });
+describe("MathUtils", function() {
     
+ 
+    beforeEach(function() {
+      
+        calc = new MathUtils(["4", "+", "5", "*", "2"]);
+        console.log(typeof(calc)+ "object");
+        spyOn(calc, 'MathSolver').andCallThrough();
+    });
+ 
+    describe("when calc is used to peform basic math operations", function(){
+         
+       
+        it("should be able to return ONLY numbers", function() {
+           
+            calc.MathSolver(["4", "+", "5", "*", "2"]);   
+        });
+        
+        it("Is it Defined??", function() {
+            
+            expect(calc.MathSolver).toBeDefined();
+            console.log(typeof(calc.MathSolver)+ "function");
+ 
+    });
+        
+        
+        it("should HAVE BEEN CALLED", function() {
+            calc.MathSolver(["4", "+", "5", "*", "2"]); 
+            expect(calc.MathSolver).toHaveBeenCalled();
+            expect(calc.MathSolver).toHaveBeenCalledWith(["4", "+", "5", "*", "2"]);
+             
+           
+    });
+      it("Does it remove operators?", function() {
+            expect(calc.MathSolver(["4", "+", "5", "*", "2"])).toEqual([4, 5, 2]);
+             
+           
+    });  
 
-});    
+});
+});
