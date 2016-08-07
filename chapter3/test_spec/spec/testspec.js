b989f06a5c4172877436706de0ccfed842633ca9
Magnus Lysfjord <magnus01lysfjord@gmail.com>
2016-08-07 17:57:38 +0100

    Check out the isNumeric Function

--- chapter3/test_spec/spec/testspec.js
+++ chapter3/test_spec/spec/testspec.js
@@ -2,9 +2,29 @@ var sol = require("../src/test2.js");
 
 console.log(sol.isNumeric.toString());
 
-
+ beforeEach(function() {
+      
+        calc = new MathUtils(["4"]);
+        console.log(typeof(calc)+ "object");
+        spyOn(calc, 'isNumeric').andCallThrough();
+    });
 
 describe("isNumeric", function() {
+
+    it("Does it contain??", function() {
+        
+        calc = new MathUtils(["4"]);
+        console.log(typeof(calc)+ "object");
+        spyOn(calc, 'isNumeric').andCallThrough();
+        expect(calc.isNumeric).toBeDefined();
+        console.log(typeof(calc.isNumeric)+ "function");
+        calc.isNumeric(["4"]); 
+        expect(calc.isNumeric).toHaveBeenCalled();
+        expect(calc.isNumeric).toHaveBeenCalledWith(["4"]);
+        var product = calc.isNumeric(4);
+    expect(product).toBe(true);
+        
+          }); 
         
 it("Does it contain??", function() {
             
@@ -14,8 +34,13 @@ it("Does it contain??", function() {
     var product = sol.isNumeric.toString();
     console.log(product);
           expect(product).toContain(["return !isNaN(parseFloat(x))"]);
+          expect(product).toContain(["function (x)"]);
+          expect(product).toContain(["return !isNaN(parseFloat(x)) && isFinite(x);"]);
 
            
          }); 
 
 });
+
+        
+        
--- chapter3/test_spec/src/test2.js
+++ chapter3/test_spec/src/test2.js
@@ -3,8 +3,32 @@
 //     return !isNaN(parseFloat(x)) && isFinite(x);
 //};
 
-isNumeric = function (x) {
-    return  isFinite(x);
+MathUtils = function() {
+    
+}
+
+MathUtils.prototype.isNumeric = function (x) {
+    return !isNaN(parseFloat(x)) && isFinite(x);
 };
 
+
+
+
+
+isNumeric = function (x) {
+    return !isNaN(parseFloat(x)) && isFinite(x);
+};
+
+
+
+
+
+
+
+
 exports.isNumeric = isNumeric;
+
+exports.MathUtils = function () {
+    return MathUtils;
+};
+
