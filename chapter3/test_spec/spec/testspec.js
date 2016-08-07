5707658f70a3eb7d1bdbf3789ff9d8bd757a96ee
Magnus Lysfjord <magnus01lysfjord@gmail.com>
2016-08-07 12:14:14 +0100

    Implemented the toContain with Mikael's string output successfully, interesting example

--- chapter3/test_spec/spec/testspec.js
+++ chapter3/test_spec/spec/testspec.js
@@ -1,3 +1,21 @@
 var sol = require("../src/test2.js");
 
-console.log(sol.isNumeric.toString());
+//var product = console.log(sol.isNumeric.toString());
+
+
+
+describe("isNumeric", function() {
+        
+it("Does it contain??", function() {
+            
+           // var product = calculator.postfixStack.toString()
+           //  var product = console.log(sol.isNumeric.toString());
+          // console.log(product+ "PRODUCT")
+    var product = sol.isNumeric.toString();
+    console.log(product);
+          expect(product).toContain(["function"]);
+
+           
+         }); 
+
+});
